536be47478a17913c866d63557d74ca6e921ad42
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-02-22 11:02:06 +0530

    128 Adding Global Colors and Editing Navigation Configuration

--- NavigationSetup.js
+++ NavigationSetup.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+
 import ManageExpense from "./screens/ManageExpnes";
 import TabSetup from "./TabSetup";
 
@@ -8,7 +9,11 @@ function NavigationSetup() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="TabSetup" component={TabSetup} />
+        <Stack.Screen
+          name="TabSetup"
+          component={TabSetup}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="ManageExpenses" component={ManageExpense} />
       </Stack.Navigator>
     </NavigationContainer>
--- TabSetup.js
+++ TabSetup.js
@@ -1,13 +1,42 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import AllExpenses from "./screens/AllExpenses";
-import ManageExpense from "./screens/ManageExpnes";
+import RecentExpenses from "./screens/RecentExpenses";
+import { GlobalStyles } from "./constants/styles";
+import { Ionicons } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator();
 function TabSetup() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="AllExpenses" component={AllExpenses} />
-      <Tab.Screen name="RecentExpenses" component={ManageExpense} />
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}
+    >
+      <Tab.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: "All Expenses",
+          tabBarLabel: "All Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: "Recent Expenses",
+          tabBarLabel: "Recent",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
