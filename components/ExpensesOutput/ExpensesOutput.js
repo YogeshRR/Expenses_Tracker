8ef7f8a50d3a1b13647fe3d26c1e15d305a18cdf
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-02-22 11:19:12 +0530

    129 Creating Key App Components to Display Expenses

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,11 @@
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+function ExpensesOutput({ expenses }) {
+  return (
+    <View>
+      <ExpensesSummary />
+      <ExpensesList />
+    </View>
+  );
+}
+export default ExpensesOutput;
---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Expenses of 7 Days</Text>
+      <Text>Total of Expenses</Text>
+    </View>
+  );
+}
+export default ExpensesSummary;
