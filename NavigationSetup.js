fc3d86e77d0cb18df757d39ead42aa63b17edf5d
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-02-23 13:22:00 +0530

    138 styling The Expense Management Screen

--- NavigationSetup.js
+++ NavigationSetup.js
@@ -3,18 +3,30 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import ManageExpense from "./screens/ManageExpnes";
 import TabSetup from "./TabSetup";
+import { GlobalStyles } from "./constants/styles";
 
 const Stack = createNativeStackNavigator();
 function NavigationSetup() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+          headerTintColor: "white",
+        }}
+      >
         <Stack.Screen
           name="TabSetup"
           component={TabSetup}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="ManageExpenses" component={ManageExpense} />
+        <Stack.Screen
+          name="ManageExpenses"
+          component={ManageExpense}
+          options={{
+            presentation: "modal",
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
